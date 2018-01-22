1e9491941ea83058f37373d12172269027aeb091
Oleg Tokarev <otokarev@gmail.com>
2018-01-23 03:46:11 +0700

    [*] updated with bifrost-js-sdk

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').load();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {
         title: 'Bifrost Demo',
-        allowHorizonHttp: process.env.ALLOW_HORIZON_HTTP,
-        env: process.env.NODE_ENV,
-        horizonUrl: process.env.HORIZON_URL,
-        bifrostUrl: process.env.BIFROST_URL,
-        issuingAccount: process.env.ISSUING_ACCOUNT,
+
+        network: 'production' === process.env.NODE_ENV ? 'live' : 'test',
+        horizonURL: process.env.HORIZON_URL,
+        bifrostURL: process.env.BIFROST_URL,
+        assetCode: process.env.ASSET_CODE,
+        price: process.env.ASSET_PRICE,
+        issuingPublicKey: process.env.ISSUING_ACCOUNT,
+        preSaleMode: process.env.PRESALE,
     });
 });
 
